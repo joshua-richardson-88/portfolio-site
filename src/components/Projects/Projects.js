7162e84a2cb0e37a575e0622f1f89151a5f29045
Joshua Richardson <flayedflesh@me.com>
2021-08-21 01:30:44 -0700

    Projects section fleshed out

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -18,6 +18,34 @@ import {
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents'
 import { projects } from '../../constants'
 
-const Projects = () => <div>Projects</div>
+const Projects = () => (
+  <Section nopadding id='projects'>
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {projects.map(({ description, id, image, source, tags, title, visit }) => (
+        <BlogCard key={id}>
+          <Img src={image} />
+          <TitleContent>
+            <HeaderThree>{title}</HeaderThree>
+          </TitleContent>
+          <CardInfo>{description}</CardInfo>
+          <div>
+            <TitleContent>Stack</TitleContent>
+            <TagList>
+              {tags.map((tag, index) => (
+                <Tag key={index}>{tag}</Tag>
+              ))}
+            </TagList>
+          </div>
+          <UtilityList>
+            <ExternalLinks href={visit}>Code</ExternalLinks>
+            <ExternalLinks href={source}>Source</ExternalLinks>
+          </UtilityList>
+        </BlogCard>
+      ))}
+    </GridContainer>
+  </Section>
+)
 
 export default Projects
