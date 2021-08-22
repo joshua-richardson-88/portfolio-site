62aa52271306bec04d42c7302a48b958ece7cee1
Joshua Richardson <flayedflesh@me.com>
2021-08-21 18:47:25 -0700

    Footer fleshed out

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -18,6 +18,35 @@ import {
   SocialIconsContainer,
 } from './FooterStyles'
 
-const Footer = () => <div>Footer</div>
+const Footer = () => (
+  <FooterWrapper>
+    <LinkList>
+      <LinkColumn>
+        <LinkTitle>Call</LinkTitle>
+        <LinkItem href='tel:111-111-1111'>111-111-1111</LinkItem>
+      </LinkColumn>
+      <LinkColumn>
+        <LinkTitle>Email</LinkTitle>
+        <LinkItem href='mailto:contact@cursedtale.com'>contact@cursedtale.com</LinkItem>
+      </LinkColumn>
+    </LinkList>
+    <SocialIconsContainer>
+      <CompanyContainer>
+        <Slogan>ADD A SLOGAN</Slogan>
+      </CompanyContainer>
+      <SocialContainer>
+        <SocialIcons href='https://github.com'>
+          <AiFillGithub size='3rem' />
+        </SocialIcons>
+        <SocialIcons href='https://linkedin.com'>
+          <AiFillLinkedin size='3rem' />
+        </SocialIcons>
+        <SocialIcons href='https://instagram.com'>
+          <AiFillInstagram size='3rem' />
+        </SocialIcons>
+      </SocialContainer>
+    </SocialIconsContainer>
+  </FooterWrapper>
+)
 
 export default Footer
